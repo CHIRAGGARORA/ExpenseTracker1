2cef9b2fbcac512b1a921c711660bfdde0428fa2
Chirag Arora <arora2002chirag@gmail.com>
2024-01-02 23:19:16 +0530

    Initial Commit

---
+++ ExpenseTracker1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ExpenseTracker1
+//
+//  Created by chirag arora on 02/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ExpenseTracker1/ExpenseTracker1App.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTracker1App.swift
+//  ExpenseTracker1
+//
+//  Created by chirag arora on 02/01/24.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTracker1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
